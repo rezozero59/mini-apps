01974de96fa4a7da4857576252dacf84768293da
rezozero59 <laurentdelassus@gmail.com>
2024-01-11 19:23:18 +0100

    Pomodoro CSS

--- src/components/Pomodoro/Pomodoro.jsx
+++ src/components/Pomodoro/Pomodoro.jsx
@@ -17,43 +17,47 @@ function Pomodoro() {
   }
 
   return (
-    <div className="bg-slate-700 text-slate-100  mt-20 rounded-lg min-h-screen">
-      <div className="max-w-xl mx-auto border border-slate-500 rounded p-10">
-        <h1 className="text-center text-3xl mb-8">Pomodoro App</h1>
+    <div className="bg-stone-100 w-full mt-20 p-20">
+      <div className="bg-slate-700 text-slate-100   rounded-xl">
+        <div className="max-w-xl mx-auto border border-slate-500 rounded p-10">
+          <h1 className="text-center text-3xl mb-8">Pomodoro App</h1>
 
-        <div className="flex justify-center mb-8">
-          <div className="mr-10">
-            <p className="text-center mb-1">Durée session</p>
-            <div className="flex">
-              <UpdateTimeButton sign={"-"} type={"session"} />
-              <p className="mx-4 text-xl">{chronoValues.session.value / 60}</p>
-              <UpdateTimeButton sign={"+"} type={"session"} />
+          <div className="flex justify-center mb-8">
+            <div className="mr-10">
+              <p className="text-center mb-1">Durée session</p>
+              <div className="flex">
+                <UpdateTimeButton sign={"-"} type={"session"} />
+                <p className="mx-4 text-xl">
+                  {chronoValues.session.value / 60}
+                </p>
+                <UpdateTimeButton sign={"+"} type={"session"} />
+              </div>
+            </div>
+
+            <div>
+              <p className="text-center mb-1">Pauses</p>
+              <div className="flex">
+                <UpdateTimeButton sign={"-"} type={"pause"} />
+                <p className="mx-4 text-xl">{chronoValues.pause.value / 60}</p>
+                <UpdateTimeButton sign={"+"} type={"pause"} />
+              </div>
             </div>
           </div>
 
-          <div>
-            <p className="text-center mb-1">Pauses</p>
-            <div className="flex">
-              <UpdateTimeButton sign={"-"} type={"pause"} />
-              <p className="mx-4 text-xl">{chronoValues.pause.value / 60}</p>
-              <UpdateTimeButton sign={"+"} type={"pause"} />
-            </div>
-          </div>
+          <p className="text-center mb-2 text-xl font-semibold">
+            {chronoValues.displayedValue.heading}
+          </p>
+          <p className="text-center flex justify-center mb-1">
+            <span className="text-4xl p-4 rounded bg-slate-300 text-slate-900">
+              {getFormattedValues(chronoValues.displayedValue.value)}
+            </span>
+          </p>
+          <p className="mb-10 text-center">
+            Cycle(s) écoulé(s) : {chronoValues.cycles}
+          </p>
+
+          <ToggleButton />
         </div>
-
-        <p className="text-center mb-2 text-xl font-semibold">
-          {chronoValues.displayedValue.heading}
-        </p>
-        <p className="text-center flex justify-center mb-1">
-          <span className="text-4xl p-4 rounded bg-slate-300 text-slate-900">
-            {getFormattedValues(chronoValues.displayedValue.value)}
-          </span>
-        </p>
-        <p className="mb-10 text-center">
-          Cycle(s) écoulé(s) : {chronoValues.cycles}
-        </p>
-
-        <ToggleButton />
       </div>
     </div>
   );
