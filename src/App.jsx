2f5d012963814f5f338c124d9f1fe7cc6fbc3cb2
rezozero59 <laurentdelassus@gmail.com>
2024-01-11 19:09:55 +0100

    pomodoro integration ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Countries from "./components/Countries/Countries";
 import Header from "./components/Header/Header";
 import IdeApp from "./components/Ide/IdeApp";
+import Pomodoro from "./components/Pomodoro/Pomodoro";
 import Slider from "./components/Slider/Slider";
 import Weather from "./components/Weather/Weather";
 
@@ -12,6 +13,7 @@ function App() {
       <Weather />
       <Countries />
       <IdeApp />
+      <Pomodoro />
 
       {/* autres applis à venir... */}
     </div>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tabs from "./components/Ide/features/tabs";
 import preview from "./components/Ide/features/preview";
+import chrono from "./components/Pomodoro/features/chrono";
 
 export const store = configureStore({
   reducer: {
     tabs,
     preview,
+    chrono,
   },
 });
