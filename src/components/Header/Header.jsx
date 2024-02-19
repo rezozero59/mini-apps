312c9532d1e0be67101c3b516525deec71522538
rezozero59 <laurentdelassus@gmail.com>
2024-02-19 19:25:50 +0100

    mico add .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <div className="container">
       <h1 className="header-title">Mini projets React by Laurent Delassus</h1>
-      <p>Décembre 2023</p>
-      <p>Petits projets d'entrainement </p>
+      <p>2023</p>
+      <p>Petits projets pour s'entrainer </p>
     </div>
   );
 }
