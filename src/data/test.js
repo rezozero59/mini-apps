7d2d76bc576c9cbb24cf8fe9070b8ff3b2343de0
rezozero59 <laurentdelassus@gmail.com>
2024-03-15 19:09:53 +0100

    prepa test

---
+++ src/data/test.js
@@ -0,0 +1,22 @@
+let slider = [
+  {
+    id: 1,
+    description: "Projet 1",
+  },
+  {
+    id: 2,
+    description: "Projet 2",
+  },
+  {
+    id: 3,
+    description: "Projet 3",
+  },
+  {
+    id: 4,
+    description: "Projet 4",
+  },
+  {
+    id: 5,
+    description: "Projet 5",
+  },
+];
